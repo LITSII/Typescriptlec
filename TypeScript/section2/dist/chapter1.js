9a9b020d24ffdb7ec37bfe48e89e89c99673212d
LITSII <jj417hom2000@naver.com>
2025-01-14 06:49:05 +0900

    section2 배열 튜플까지!

---
+++ TypeScript/section2/dist/chapter1.js
@@ -0,0 +1,29 @@
+// number
+let num1 = 123;
+let num2 = -123;
+let num3 = 0.123;
+let num4 = -0.123;
+let num5 = Infinity;
+let num6 = -Infinity;
+let num7 = NaN;
+// string
+let str1 = "hello";
+let str2 = 'hello';
+let str3 = `hello`;
+let str4 = `hello ${num1}`;
+str1.toUpperCase;
+//boolean
+let bool1 = true;
+let bool2 = false;
+//null
+let null1 = null;
+//undefined
+let undefined1 = undefined;
+//let numA: number = null;
+// literal type
+// literal = value;
+let numA = 10;
+let strA = "str";
+export {};
+//let boolA: true = false;
+//numA = 12;
